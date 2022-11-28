0a343aaa2e0e28ab97d1a07dbd5f7706f57fcb76
MarkusMaal2 <markus.maal@voco.ee>
2022-11-28 13:34:47 +0200

    Change weather data

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const weather = new Weather("Tallinn")
 // UI object
 const ui = new UI()
 
+// events
+const form = document.querySelector("#change-city")
+form.addEventListener("submit", changeWeather)
+
 // get city weather data
 function getWeather() {
     weather.getWeather()
@@ -11,6 +15,15 @@ function getWeather() {
             ui.drawWeather(data)
         })
         .catch(error => console.log(error))
+}
+
+// to change weather
+function changeWeather(e) {
+    const city = document.querySelector("#city-name").value
+    weather.changeCity(city)
+    document.querySelector("#city-name").value = "";
+    getWeather()
+    e.preventDefault()
 }
 
 getWeather()
